fc72c437fb5b3bf9c63018f7d2c2bbde6cb346d9
ardaalkan <ardaaa.alk@gmail.com>
2023-08-20 16:48:36 +0300

    setted list page and init placeholder

--- src/features/ListPage/List.jsx
+++ src/features/ListPage/List.jsx
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./List.module.css";
 import ListTable from "./ListTable/ListTable";
 
 const List = () => {
+  const [filteredInfo, setFilteredInfo] = useState({});
+  const [sortedInfo, setSortedInfo] = useState({});
+  const [originAirport, setOriginAirport] = useState("");
+  const [destinationAirport, setDestinationAirport] = useState("");
+  const [tableData, setTableData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const origin = localStorage.getItem("originAirport");
+    const destination = localStorage.getItem("destinationAirport");
+
+    if (origin && destination) {
+      fetchData(origin, destination);
+    }
+  }, []);
+
+  const fetchData = async (origin, destination) => {
+    try {
+      const response = await fetch("http://localhost:3001/flights");
+      const data = await response.json();
+
+      const filteredData = data.filter(
+        (item) =>
+          item.originAirport.code === origin &&
+          item.destinationAirport.code === destination
+      );
+
+      setTableData(filteredData);
+      setLoading(false);
+    } catch (error) {
+      console.log("Error:", error);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className={styles.main}>
-      <ListTable />
+
+      <ListTable tableData={tableData} />
     </div>
   );
 };
